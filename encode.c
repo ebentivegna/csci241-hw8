887758ba44dbc41c72a73884fbc2fbd5fe1078fc
Paul Buser <pbuser@clyde>
2015-04-26 15:29:32 -0400

    Added EOF to linked list

--- encode.c
+++ encode.c
@@ -21,41 +21,43 @@ void char_count(int* frequencies, FILE* source) {
 }
 
 /* Mallocs and returns a sorted (ascending) linked list of Nodes
+ * Includes EOF character with frequency of 1
  * @param frequencies: an array of size 256 representing frequencies of each ASCII character
  * @return the head of the list
  */
 Node* list_maker(int* frequencies) {
 
+    // Add EOF with frequency 1
 	Node* head = malloc(sizeof(Node));
-	head->char_val = 0;
-    head->frequency = frequencies[0];
-    head->left = NULL;
-    head->right = NULL;
-    head->next = NULL;
-    Node* current = head;
+	head->char_val = EOF;
+	head->frequency = 1;
+	head->left = NULL;
+	head->right = NULL;
+	head->next = NULL;
+	Node* current = head;
 	//go thru frequency list and make linked list in order of frequency count
-	for (int i = 1; i < ASCII_LEN; i++) {
+	for (int i = 0; i < ASCII_LEN; i++) {
         // Create node for the element we're adding
-		Node* new_node = malloc(sizeof(Node));
-        new_node->char_val = i;
-        new_node->frequency = frequencies[i];
-        new_node->left = NULL;
-        new_node->right = NULL;
-        new_node->next = NULL;
+	    Node* new_node = malloc(sizeof(Node));
+	    new_node->char_val = i;
+	    new_node->frequency = frequencies[i];
+	    new_node->left = NULL;
+	    new_node->right = NULL;
+	    new_node->next = NULL;
 
-        // Insert our node into the list at the correct position
-		current = head; // Jump to the beginning of the list
-        if (new_node->frequency > head->frequency)
-        {
-            while (NULL != current->next && new_node->frequency > current->next->frequency) {
-                current = current->next;
-            }
-            new_node->next = current->next;
-            current->next = new_node;
-        } else {
-            new_node->next = head;
-            head = new_node;
-        }
+	    // Insert our node into the list at the correct position
+	    current = head; // Jump to the beginning of the list
+	    if (new_node->frequency > head->frequency)
+	    {
+		while (NULL != current->next && new_node->frequency > current->next->frequency) {
+		    current = current->next;
+		}
+		new_node->next = current->next;
+		current->next = new_node;
+	    } else {
+		new_node->next = head;
+		head = new_node;
+	    }
 	}
     return head;
 }
